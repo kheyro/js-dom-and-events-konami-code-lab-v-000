0d2306cb86354c18b179c055b0e7320b864db2af
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 19:13:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,21 @@ let index = 0
 
 function init() {
   // Write your JavaScript code inside the init() function
+  document.addEventListener('keydown', onKeyDownHandler(e))
+}
 
-  
-  
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
