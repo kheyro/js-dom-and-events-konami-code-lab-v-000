adab2abf55b79cd6ee72a018ce05dcb1e6137678
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 19:15:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0
 
 function init() {
+  let index = 0
   // Write your JavaScript code inside the init() function
   document.addEventListener('keydown', onKeyDownHandler(e))
 }
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
-  if (key === alphabet[index]) {
+
+  if (key === code[index]) {
     index++;
 
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
 
       index = 0;
